dc295fa73cfff9535aacbb0f82c8da804701f043
Kenny4487 <43713350+Kenny4487@users.noreply.github.com>
2025-02-24 00:22:15 +0100

    Add files via upload

--- cbpi4_AJ_SR04M/__init__.py
+++ cbpi4_AJ_SR04M/__init__.py
@@ -23,7 +23,7 @@ class AJ_SR04M(CBPiSensor):
         self.interval = int(self.props.get("Interval", 2))
         self.trig_pin = int(self.props.get("Ultrasonic Trig Pin", 23))
         self.echo_pin = int(self.props.get("Ultrasonic Echo Pin", 24))
-        self.pot_diameter = float(self.props.get("Pot Diameter", 30))
+        self.pot_diameter = float(self.props.get("Pot Diameter", 40))
         self.mounting_height = float(self.props.get("Mounting Height", 50))
         
         GPIO.setmode(GPIO.BCM)
@@ -39,25 +39,27 @@ class AJ_SR04M(CBPiSensor):
             await asyncio.sleep(self.interval)
 
     def measure_distance(self):
-        GPIO.output(self.trig_pin, True)
-        time_start = time.perf_counter()
-        while time.perf_counter() - time_start < 0.00001:
-            pass
-        GPIO.output(self.trig_pin, False)
-        
-        start_time, stop_time = None, None
-        
-        while GPIO.input(self.echo_pin) == 0:
-            start_time = time.perf_counter()
-        while GPIO.input(self.echo_pin) == 1:
-            stop_time = time.perf_counter()
-        
-        if start_time and stop_time:
-            time_elapsed = stop_time - start_time
-            distance = (time_elapsed * 34300) / 2
-            return round(distance, 2)
-        else:
-            return None  # Return None if measurement failed
+        try:
+            GPIO.output(self.trig_pin, True)
+            time.sleep(0.00001)
+            GPIO.output(self.trig_pin, False)
+            
+            start_time, stop_time = None, None
+            
+            while GPIO.input(self.echo_pin) == 0:
+                start_time = time.perf_counter()
+            while GPIO.input(self.echo_pin) == 1:
+                stop_time = time.perf_counter()
+            
+            if start_time and stop_time:
+                time_elapsed = stop_time - start_time
+                distance = (time_elapsed * 34300) / 2
+                return round(distance, 2)
+            else:
+                return None  # Return None if measurement failed
+        except Exception as e:
+            glogger.error(f"Error measuring distance: {e}")
+            return None
     
     def calculate_volume(self, distance):
         if distance is None:
@@ -71,4 +73,4 @@ class AJ_SR04M(CBPiSensor):
         return dict(value=self.value)
 
 def setup(cbpi):
-    cbpi.plugin.register("AJ-SR04M Distance Sensor", AJ_SR04M)
+    cbpi.plugin.register("AJ-SR04M Volume Measurement", AJ_SR04M)
